4d005c54c44f61a2a1bc50a48211946dea264188
Shariq Khan <khanshariq76543@gmail.com>
2024-06-22 22:01:42 +0530

    Update config.js

--- config.js
+++ config.js
@@ -3,6 +3,5 @@ module.exports = {
   SERVER_TYPE: process.env.NODE_ENV,
   JWT_SECRET: process.env.JWT_SECRET,
   IS_PRODUCTION: process.env.NODE_ENV === "production",
-  DEFAULT_PROFILE_IMAGE: `https://static.vecteezy.com/system/resources/previews/009/292/244/original/default-avatar-icon-of-social-media-user-vector.jpg`,
-  DEFAULT_POST_IMAGE: `https://www.hostinger.com/tutorials/wp-content/uploads/sites/2/2021/09/how-to-write-a-blog-post.png`,
+  SERVER_PORT: process.env.PORT,
 };
