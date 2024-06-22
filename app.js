3404ab4a1b89c20cdaeea158e11c61b706c042c4
Shariq Khan <khanshariq76543@gmail.com>
2024-06-22 21:58:54 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,16 +1,29 @@
 const express = require("express");
+const app = express();
+const cors = require("cors");
+const http = require("http");
+const server = http.createServer(app);
 const mainRoutes = require("./routes/main.route");
 const { globalErrorHandler, noRouteFound } = require("./utils/error.util");
 const cookieParser = require("cookie-parser");
+const mountSocket = require("./services/socket.service");
 
-const app = express();
+//socket mount
+const io = mountSocket(server);
+
+//middleware to attach io to every request
+app.use((req, res, next) => {
+  req.io = io;
+  next();
+});
 
 //middlewares
+app.use(cors());
 app.use(cookieParser());
 app.use(express.json());
 
 //main routes
-app.use("/api", mainRoutes);
+app.use("/api/v1", mainRoutes);
 
 //global error handler
 app.use(globalErrorHandler);
@@ -18,4 +31,4 @@ app.use(globalErrorHandler);
 //no route found
 app.use(noRouteFound);
 
-module.exports = app;
+module.exports = server;
